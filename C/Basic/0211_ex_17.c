7632a7943d8bedd2d1c19883e9f81d474d6e94a2
warrocking <115214928+warrocking@users.noreply.github.com>
2026-02-11 16:41:34 +0900

    0211_4시42분 수업 끝난 후 커밋.

--- C/Basic/0211_ex_17.c
+++ C/Basic/0211_ex_17.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-int factorial(int n)
+/*
+// 재귀식
+ int factorial(int n)
 {
     if (n <= 1)
     {
@@ -14,7 +15,28 @@ int factorial(int n)
 
 int main()
 {
-    int n = 5;
+    int n = 10;
     printf("%d\n", factorial(n));
+    return 0;
+}
+
+
+*/
+
+// 점화식
+
+int main()
+{
+    int n = 5;
+    int dp[10];
+
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = i * dp[i - 1];
+        printf("dp[%d] = %d\n", i, dp[i]);
+    }
+
     return 0;
 }
